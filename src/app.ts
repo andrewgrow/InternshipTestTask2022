9ef62f52c518ee2ca42b2624056b8cbc68a13e51
Andrew G <a+github@gahov.com>
2022-09-27 15:12:23 +0300

    enable eslint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import * as express from 'express';
-import { ratesRouter } from "./routes/rates";
+import { ratesRouter } from './routes/rates';
 
 const app = express();
 app.use('/', ratesRouter);
 
-const port: String = process.env.PORT || '3000';
+const port = process.env.PORT ?? 3000;
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
 
 export {};
--- src/routes/rates.ts
+++ src/routes/rates.ts
@@ -1,4 +1,4 @@
-import * as express from "express";
+import * as express from 'express';
 import { RatesController } from '../controllers/rates';
 
 const ratesController = new RatesController();
